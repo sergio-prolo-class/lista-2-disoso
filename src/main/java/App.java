8283b3a2a970f041d2d4c377c8cd54889fd0f76c
disfoggia <143456100+disfoggia@users.noreply.github.com>
2025-05-14 19:57:06 -0300

    feita etapa1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,1 +1,23 @@
+import edu.princeton.cs.algs4.Draw;
 
+public class App {
+    // Adicionando tela, objeto que vamos usar para desenhar
+    private Draw tela;
+
+    public App() {
+        tela = new Draw();
+    // Dimensao da janela (conforme lista)
+        tela.setCanvasSize(1000, 600);
+    // Definindo a escala do eixo X da janela (conforme lista)
+        tela.setXscale(0, 1000);
+    // Definindo a escala do eixo Y da janela (conforme lista)
+        tela.setYscale(0, 600);  
+    }
+
+    public static void main(String[] args) {
+    // Criando um objeto App (conforme lista)
+        App aplicativo = new App(); 
+    // Mostrando a janela criada
+        aplicativo.tela.show(); 
+    }
+}
