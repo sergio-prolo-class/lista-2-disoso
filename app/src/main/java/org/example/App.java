c08fdf9d5f14cc9d51e9bc16803e48d6fdeacfb8
Sophia Ventura <sophia.a2003@aluno.ifsc.edu.br>
2025-05-14 22:27:42 -0300

    parte1

---
+++ app/src/main/java/org/example/App.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import edu.princeton.cs.algs4.Draw;
+
+public class App {
+    // Adicionando tela, objeto que vamos usar para desenhar
+    private Draw tela;
+
+    public App() {
+        tela = new Draw();
+        // Dimensao da janela (conforme lista)
+        tela.setCanvasSize(1000, 600);
+        // Definindo a escala do eixo X da janela (conforme lista)
+        tela.setXscale(0, 1000);
+        // Definindo a escala do eixo Y da janela (conforme lista)
+        tela.setYscale(0, 600);
+    }
+
+
+    public static void main(String[] args) {
+        App aplicativo = new App();
+        Grade grade = new Grade(100,100);
+        aplicativo.tela.show();
+    }
+}
+
+
+
+
+
